5e0a638eef573d0793bbfb44505eae8e359f371b
rostyslav-berezhnytskyi <rostik2510@gmail.com>
2025-12-13 00:14:24 +0200

    Start to add a RAG logic to the code

--- src/main/java/com/eazybytes/openai/controller/ChatMemoryController.java
+++ src/main/java/com/eazybytes/openai/controller/ChatMemoryController.java
@@ -4,10 +4,9 @@ import com.eazybytes.openai.advisors.TokenUsageAuditAdvisor;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.ai.chat.memory.ChatMemory.CONVERSATION_ID;
 
 @RestController
 @RequestMapping("/api")
@@ -20,7 +19,10 @@ public class ChatMemoryController {
     }
 
     @GetMapping("/chat-memory")
-    public ResponseEntity<String> chatMemory(@RequestParam("message") String message) {
-        return ResponseEntity.ok(chatClient.prompt().user(message).call().content());
+    public ResponseEntity<String> chatMemory(@RequestHeader("username") String username, @RequestParam("message") String message) {
+        return ResponseEntity.ok(chatClient.prompt().user(message).advisors(
+                    advisorSpec -> advisorSpec.param(CONVERSATION_ID, username)
+                )
+                .call().content());
     }
 }
